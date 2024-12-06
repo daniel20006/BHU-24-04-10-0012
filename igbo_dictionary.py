86e1cbae2826c12152c98546a99450e85244b075
daniel20006 <danielokeke602@gmail.com>
2024-12-06 23:29:19 +0100

    Add files via upload

---
+++ igbo_dictionary.py
@@ -0,0 +1,9 @@
+print("Igbo Dictionary")
+print("---------------")
+igbo_dictionary ={
+        'bia':'come',
+        'biko' : 'please',
+        'hapia': 'leave it'
+    }
+word = input('enter an igbo word:')
+print(igbo_dictionary[word])
---
+++ numbers.py
@@ -0,0 +1,9 @@
+
+print('-----------------')
+numbers = list(range(1,10))
+total=sum(numbers)
+print('numbers:',numbers)
+print('sum of numbers:', total)
+
+for number in range (10,0,-1):
+    print(number)
